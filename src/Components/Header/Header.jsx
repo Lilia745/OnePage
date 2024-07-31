8fa368c63c84b28aae189d47eb3569940f56496b
Lilia Hasratyan <hasratyanlilia@gmail.com>
2024-07-31 23:26:18 +0400

    added header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header() {
+    return(
+        <div className="cont">
+            <header>
+                <div>
+                    <a href="">OnePage</a>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default Header
