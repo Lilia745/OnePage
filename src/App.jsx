664ba6527171562abf55ee14dd887e367631d224
Lilia Hasratyan <hasratyanlilia@gmail.com>
2024-08-01 00:05:00 +0400

    added section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./Components/Header/Header"
+import Main from './Components/Section/Main';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   );
 }
---
+++ src/Components/Section/Main.jsx
@@ -0,0 +1,13 @@
+function Main() {
+    return(
+        <div className="box">
+            <div className="main">
+                <h1>One Page Bootstrap</h1>
+                <h1>Website Template</h1>
+                <p>We are team of talented designers making websites with</p>
+                <p>Bootstrap</p>
+            </div>
+        </div>
+    );
+}
+export default Main
